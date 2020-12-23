2576cdda29cce61b14d7237e5779c094a9f2fec0
mondobizz <caseyjhatley@yahoo.com>
2020-12-22 19:18:46 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+import GameBoard from './components/GameBoard';
+import ColorPicker from './components/ColorPicker';
+import GameTimer from './components/GameTimer';
+import NewGameButton from './components/NewGameButton';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">React Mastermind</header>
+      <GameBoard />
+      <ColorPicker />
+      <GameTimer />
+      <NewGameButton />
+      <Footer />
     </div>
   );
 }
